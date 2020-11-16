5495f5819171d7e5ec718f12c91b5bd911a973ba
Derik <derik@oagro.com.br>
2020-11-16 18:14:44 -0300

    Adjusting weather file to get data from current location and by city

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,31 @@
+import 'package:clima/services/networking.dart';
+import 'package:clima/services/location.dart';
+
+const kApiKey = 'bad9afa863e4a08839883e0e32d95f61';
+const kOpenWeatherURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+Location location = Location();
+
 class WeatherModel {
+  Future getCityWeather(String city) async {
+    String url = '$kOpenWeatherURL?q=$city&appid=$kApiKey&units=metric';
+
+    NetWorkHelper netWorkHelper = NetWorkHelper(url);
+
+    var weatherData = await netWorkHelper.getData();
+    return weatherData;
+  }
+
+  Future getDataWeather() async {
+    await location.getCurrentLocation();
+    String url = '$kOpenWeatherURL?lat=${location.latitude}&lon=${location.longitude}&appid=$kApiKey&units=metric';
+
+    NetWorkHelper netWorkHelper = NetWorkHelper(url);
+
+    var weatherData = await netWorkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
